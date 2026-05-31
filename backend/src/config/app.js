c8c0dd0d09512d4f55f85d1282bd6566b85248ea
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-05-31 13:51:30 +0200

    Uncomment API route definitions in app.js

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -29,12 +29,12 @@ app.get("/", (req, res) => {
 });
 
 //Definición de rutas
-//app.use("/api/auth", authRoutes);
-//app.use("/api/users", userRoutes);
-//app.use("/api/cards", cardRoutes);
-//app.use("/api/settings", settingsRoutes);
-//app.use("/api/admin", adminRoutes);
-//app.use("/api/tags", tagRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/cards", cardRoutes);
+app.use("/api/settings", settingsRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/tags", tagRoutes);
 
 app.use((err, req, res, next) => {
   console.error("Error:", err);
