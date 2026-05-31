172017594e5c32460da45f909061133a0de04419
Ivan <ivanatienzamoya@gmail.com>
2026-05-31 13:03:22 +0200

    Puerto cambiado para despliegue backend railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 //Arranque del servidor
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
