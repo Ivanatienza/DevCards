2bb3bfb01d454d48f7ff0488984d95a655e69651
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-02 19:36:01 +0200

    Update app.js

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://dev-cards-ruddy.vercel.app/',
+  origin: 'https://dev-cards-ruddy.vercel.app',
   credentials: true
 }));
 
