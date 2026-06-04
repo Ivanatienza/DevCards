fa30862274db597bae2d4234a042dce102b4c7aa
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-04 21:10:10 +0200

    Refactor CardList.vue template structure

--- frontend/src/components/CardList.vue
+++ frontend/src/components/CardList.vue
@@ -1,26 +1,25 @@
 <template>
 
-<div v-if="cards?.length" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+  <div v-if="cards?.length" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
-  <CardItem
-    v-for="card in cards"
-    :key="card.id"
-    :card="card"
-    :readonly="readonly"
-    @edit="emit('edit', $event)"
-    @delete="emit('delete', $event)"
-  />
+    <CardItem
+      v-for="card in cards"
+      :key="card.id"
+      :card="card"
+      :readonly="readonly"
+      @edit="emit('edit', $event)"
+      @delete="emit('delete', $event)"
+    />
 
-</div>
+  </div>
 
-<p v-else class="text-center text-gray-500">
-  {{ t("noCards") }}
-</p>
+  <p v-else class="text-center text-gray-500">
+    {{ t("noCards") }}
+  </p>
 
 </template>
 
 <script setup>
-
 import { useI18n } from "vue-i18n";
 import CardItem from "./CardItem.vue";
 
@@ -31,7 +30,6 @@ defineProps({
     type: Array,
     default: () => []
   },
-  // readonly: oculta los botones Editar/Eliminar
   readonly: {
     type: Boolean,
     default: false
@@ -39,5 +37,5 @@ defineProps({
 });
 
 const emit = defineEmits(["edit", "delete"]);
-
+  
 </script>
