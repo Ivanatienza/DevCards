2182c037674634f31a42911cdf7e3af68f639c66
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-04 20:13:37 +0200

    Refactor card routes to simplify access control
    Removed token verification for user card routes and reorganized route definitions.

--- backend/src/routes/cardRoutes.js
+++ backend/src/routes/cardRoutes.js
@@ -1,21 +1,23 @@
 import express from "express";
 import { createCard, getCards, getPublicCards, getCardById, updateCard, removeCard } from "../controllers/cardController.js";
+
 import { verifyToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-//Definición de rutas protegidas de cards
-
+/* =========================
+   PUBLIC CARDS
+========================= */
 router.get("/public", getPublicCards);
+router.get("/public/:id", getCardById);
 
-router.get("/", verifyToken, getCards);
-
-router.get("/:id", verifyToken, getCardById);
-
-router.post("/", verifyToken, createCard);
-
-router.put("/:id", verifyToken, updateCard);
-
-router.delete("/:id", verifyToken, removeCard);
+/* =========================
+   USER CARDS
+========================= */
+router.get("/", getCards);
+router.get("/:id", getCardById);
+router.post("/", createCard);
+router.put("/:id", updateCard);
+router.delete("/:id", removeCard);
 
 export default router;
