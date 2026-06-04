efeb688d936ba5b18ceefdba4699abe4d136f226
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-04 11:18:55 +0200

    Fix import path casing for Button component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -70,7 +70,7 @@ import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useToast } from "vue-toastification";
 import { useI18n } from "vue-i18n";
-import Button from "../components/ui/Button.vue";
+import Button from "../components/UI/Button.vue";
 
 const router = useRouter();
 const toast = useToast();
