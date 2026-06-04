131d1d555afcf1f3daec559efc95d19cfd984419
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-04 11:34:55 +0200

    Update CardForm.vue

--- frontend/src/components/CardForm.vue
+++ frontend/src/components/CardForm.vue
@@ -74,7 +74,7 @@
 
 import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
-import Button from "./ui/Button.vue";
+import Button from "./UI/Button.vue";
 
 const { t } = useI18n();
 
