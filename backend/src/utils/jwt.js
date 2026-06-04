79bd05e1a8a71d90875432befcee1cdf83e6b37c
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-04 13:13:36 +0200

    Include email in JWT token generation

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 //Generación del token
 export const generateToken = (user) => {
     return jwt.sign(
-        {id: user.id, role: user.role},
+        {id: user.id, email: user.email, role: user.role},
         process.env.JWT_SECRET,
         {expiresIn: "1d"}
     );
