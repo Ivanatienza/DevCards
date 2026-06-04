0b53b49a8c2d89387a6ebcdb8c630b10f33b565f
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-04 12:39:20 +0200

    Test database connection on server startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 //Punto de entrada del servidor
 import app from "./config/app.js";
+import { testConnection } from "./config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+await testConnection();
 
 //Arranque del servidor
 app.listen(PORT, () => {
