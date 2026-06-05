316e0477a067ca5ceb9c48a59d7d90d9f2e9edaf
Ivan_Atienza <151560367+Ivanatienza@users.noreply.github.com>
2026-06-05 08:43:12 +0200

    Add token verification to card routes
    Added token verification middleware to card routes.

--- backend/src/routes/cardRoutes.js
+++ backend/src/routes/cardRoutes.js
@@ -14,10 +14,10 @@ router.get("/public/:id", getCardById);
 /* =========================
    USER CARDS
 ========================= */
-router.get("/", getCards);
-router.get("/:id", getCardById);
-router.post("/", createCard);
-router.put("/:id", updateCard);
-router.delete("/:id", removeCard);
+router.get("/", verifyToken, getCards);
+router.get("/:id", verifyToken,, getCardById);
+router.post("/", verifyToken,, createCard);
+router.put("/:id", verifyToken,, updateCard);
+router.delete("/:id", verifyToken,, removeCard);
 
 export default router;
